494b2bd9d3cea6fb7543e53b5b554ff8d4681a34
Anshu <gxanshu@yahoo.com>
2026-07-18 14:52:26 +0530

    feat: talk with default keyring manager to store passwords

---
+++ src/postbox/core/secrets.py
@@ -0,0 +1,28 @@
+import gi
+
+gi.require_version("Secret", "1")
+
+from gi.repository import Secret
+
+_SCHEMA = Secret.Schema.new(
+    "in.gxanshu.postbox.Account",
+    Secret.SchemaFlags.NONE,
+    {"account-id": Secret.SchemaAttributeType.INTEGER}
+)
+
+def store_password(account_id: int, password: str) -> None:
+    Secret.password_store_sync(
+        _SCHEMA,
+        {"account-id": account_id}
+        Secret.COLLECTION_DEFAULT,
+        f"Postbox account {account_id}"
+        password,
+        None
+    )
+
+def lookup_password(account_id: int) -> str | None:
+    return Secret.password_lookup_sync(_SCHEMA, {"account-id": account_id}, None)
+
+
+def clear_password(account_id: int) -> str | None:
+    return Secret.password_clear_sync(_SCHEMA, {"account-id": account_id}, None)
